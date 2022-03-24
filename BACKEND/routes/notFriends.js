b33e7742ce1fa6cf982b84729796f676ee586cba
Daniel Velásquez <jdvela158@gmail.com>
2022-03-24 06:41:15 -0600

    Query for not friends

--- BACKEND/routes/notFriends.js
+++ BACKEND/routes/notFriends.js
@@ -13,15 +13,10 @@ connection.connect(err => {
 router.get('/notFriends/:usuario', (req, res) => {
     const { usuario } = req.params; 
 
-    const sql = `SELECT * FROM USUARIO
-                  LEFT JOIN (
-                      SELECT id_usuario2
-                      FROM USUARIO
-                      LEFT JOIN amistad a on USUARIO.id_usuario = a.id_usuario1
-                      WHERE id_usuario1 = 4
-                  ) U
-                  ON USUARIO.id_usuario = U.id_usuario2
-                  WHERE U.id_usuario2 is null AND USUARIO.usuario != '${usuario}';`;
+    const sql = `SELECT * FROM ( SELECT id_usuario2 FROM USUARIO
+                 INNER JOIN amistad a on USUARIO.id_usuario = a.id_usuario1
+                 WHERE usuario = '${ usuario }' AND aceptar = 0 ) as U
+                 INNER JOIN USUARIO on USUARIO.id_usuario = U.id_usuario2;`;
     
 
     connection.query(sql, (error, results) => {
